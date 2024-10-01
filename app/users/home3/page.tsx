f6fcba2dc3f463901ba26c042b7a6cfa6e15881d
AdwinTS <adwintsunil8@gmail.com>
2024-10-01 22:28:31 +0530

    hello to it is somewhat okay

--- app/users/home3/page.tsx
+++ app/users/home3/page.tsx
@@ -96,7 +96,7 @@ const page = () => {
           </div>
           <div className="mb-4">
             <label className="block text-left text-lg font-semibold mb-2" htmlFor="message">Your Message</label>
-            <textarea id="message" name="message" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" placeholder="Write your message" rows="5"></textarea>
+            <textarea id="message" name="message" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" placeholder="Write your message" rows={5}></textarea>
           </div>
           <button type="submit" className="w-full bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600 focus:outline-none">Send Message</button>
         </form>
